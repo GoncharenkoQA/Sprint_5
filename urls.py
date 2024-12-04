f11ce58a2bd9a443ec3aea9d79a0b243bf8a3beb
GoncharenkoQA <kirakobraooo@gmail.com>
2024-12-04 17:28:55 +0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,15 @@
+class Urls:
+    # Главная страница
+    url_main_paige = "https://stellarburgers.nomoreparties.site/"
+
+    # Страница входа
+    url_login = "https://stellarburgers.nomoreparties.site/login"
+
+    #  Личный кабинет -> Профиль
+    url_profile = "https://stellarburgers.nomoreparties.site/account/profile"
+
+    # Страница регистрации
+    url_register = "https://stellarburgers.nomoreparties.site/register"
+
+    # Странца восстановления пароля
+    url_forgot_password = "https://stellarburgers.nomoreparties.site/forgot-password"
